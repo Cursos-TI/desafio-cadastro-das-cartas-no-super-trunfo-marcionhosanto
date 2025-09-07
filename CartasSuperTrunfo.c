aff263c0004a9a482195ceb538a6b329837e7e69
marcionhosanto <marciosilvasanto@hotmail.com>
2025-09-07 04:22:42 +0000

    CartasSuperTrunfo 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,33 +1,36 @@
-#include <stdio.h>
-int main() {
-    
-    char codigo[30];
-    char estado[30];
-    int populacao;
-    float area;
-    float pib;
-    int turisticos;
+	#include <stdio.h>
 
-    	printf("digite codigo: \n");
-	    scanf("%s", codigo); 
-
-        printf("digite seu estado: \n");
-	    scanf("%s", estado);
-
-    	printf(" digite quantidade populacao \n");
-	    scanf("%d", &populacao);
-
-        printf(" digite area habitada \n");
-	    scanf("%f", &area);
-
-        printf("digite PIB da cidade \n");
-	    scanf("%f", &pib);
-
-        printf("digite Area turisticos \n");
-	    scanf("%d", &turisticos);
-
-        printf("codigo %s - estado: %s - populacao: %d - area: %fKm - pib: %fmilhoes - turistico %d", codigo, estado, populacao, area, pib, turisticos);
-
-        return 0;
+int main(){
+	
+	char estado[30] = "A";
+	char codigo[30] = "A01";
+	char cidade[30] = "Saopaulo";
+	int populacao = 12325000;
+	float area = 1521.11;
+	float pib = 699.28;
+	int turistico = 50;
+	
+		
+	printf("estado: %s \n", estado);
+	
+	printf("codigo: %s \n",codigo);
+	
+	printf("cidade: %s \n",cidade);
+	
+	printf("populacao: %d \n",populacao);
+		
+	printf("area: %.2f \n",area);
+	
+	printf("PIB: %.2f\n",pib);
+	
+	printf("turistico: %d \n", turistico);
+	
+	  
+	
+	printf("seu codigo: %s - estado: %s - cidade: %s - populacao: %d - area habitada: %.2f km  -  Valor PIB da cidade: %.2f milhoes - Area turistica: %d" , codigo, estado, cidade, populacao, area, pib, turistico);
+	
+	
+		
+	return 0;
         
 }
